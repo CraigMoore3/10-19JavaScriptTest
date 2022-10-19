dc1450a3327e266c8b9f8bab436d1c27e3a6e69c
Craig <cdm21d@fsu.edu>
2022-10-19 14:21:42 -0400

    In class demo

--- sketch.js
+++ sketch.js
@@ -1,7 +1,22 @@
+let canvasWidth = 800;
+let canvasHeight = 800;
+let fillColor = "black";
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(canvasWidth, canvasHeight);
+  frameRate(30)
+  angleMode(DEGREES)
 }
 
 function draw() {
-  background(125);
+  let r = random(100, 150);
+  let g = random(255);
+  let b = random(200,255);
+  background(0);
+
+  fill(map(mouseX,0,canvasWidth,0,255));
+  for (let n=0; n< 10; n++ ) {
+    rotate(frameCount/2);
+    rect(40*n, mouseY, 30, 30);
+  }
 }
